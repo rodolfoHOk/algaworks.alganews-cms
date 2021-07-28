635010b8a96f3e9b122e5ed3bc03ded2bf942db4
rodolfoHOk <rodolfo.okino@gmail.com>
2021-07-28 15:49:11 -0300

    instalando e configurando o react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route path={'/home'} exact>
+          <App />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
