90c05f1f1ef51639774eb101cf20c70328f435c3
rodolfoHOk <rodolfo.okino@gmail.com>
2021-08-04 23:33:15 -0300

    transformando info e confirm em macrotask(event loop)

--- src/app/layouts/Default/Default.layout.tsx
+++ src/app/layouts/Default/Default.layout.tsx
@@ -3,6 +3,7 @@ import * as DL from './Default.layout.styles';
 import Logo from '../../components/Logo';
 import SessionController from '../../components/SessionController/SessionController';
 import confirm from '../../../core/utils/confirm';
+import info from '../../../core/utils/info';
 
 interface DefaultLayoutProps {
   children: React.ReactNode;
@@ -27,7 +28,19 @@ function DefaultLayout(props: DefaultLayoutProps) {
           avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNf0vAZLggJoZxGKpfOa3EBClHkwQmmvv9Lg&usqp=CAU"
           onLogout={() => {
             confirm({
-              title: 'Deseja realmente sair?'
+              title: 'Deseja realmente sair?',
+              onConfirm: () => {
+                info({
+                  title: 'Deslogado',
+                  description: 'Vocẽ confirmou o logout, e será redirecionado para a página de login'
+                });
+              },
+              onCancel: () => {
+                info({
+                  title: 'Cancelado',
+                  description: 'Vocẽ cancelou o logout'
+                });
+              }
             })
           }}
         />
