7119f31a863caeeb273f32c5d077f7483ab6511c
rodolfoHOk <rodolfo.okino@gmail.com>
2021-08-12 22:55:07 -0300

    snippet para abrir links no MarkdownEditor em nova aba

--- src/app/components/MarkdownEditor/MarkdownEditor.tsx
+++ src/app/components/MarkdownEditor/MarkdownEditor.tsx
@@ -12,6 +12,22 @@ MdEditor.unuse(Plugins.FontUnderline);
 
 const parser = new MarkdownIt();
 
+const defaultRender = parser.renderer.rules.link_open ||
+  function (tokens: any, idx: any, options: any, env: any, self: any) {
+    return self.renderToken(tokens, idx, options);
+  };
+
+parser.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  var aIndex = tokens[idx].attrIndex('target');
+  if (aIndex < 0) {
+    tokens[idx].attrPush(['target', '_blank']);
+  } else {
+    //@ts-ignore
+    tokens[idx].attrs[aIndex][1] = '_blank';
+  }
+  return defaultRender(tokens, idx, options, env, self);
+};
+
 export default function MarkdownEditor(props: MarkdownEditorProps) {
   return <MdEditor
     readOnly={props.readOnly}
