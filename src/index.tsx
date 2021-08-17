2b99f38b669fa8ed7d0b1106093f6c0b19887b55
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-08-17 16:26:22 -0300

    configurando a store raiz

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,15 @@ import './core/imports.css';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyles from './core/globalStyles';
 import App from './app';
+import { Provider } from 'react-redux';
+import store from './core/store';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
     <GlobalStyles />
   </React.StrictMode >,
   document.getElementById('root')
