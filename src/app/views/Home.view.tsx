2505860e2fb253ff9b7b6d93b357bb90dd3e7e4c
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-08-18 20:05:51 -0300

    abstraindo o redux o hook usePosts

--- src/app/views/Home.view.tsx
+++ src/app/views/Home.view.tsx
@@ -5,29 +5,20 @@ import PostsList from "../features/PostsList";
 import UserTopTags from "../features/UserTopTags";
 import UserEarnings from "../features/UserEarnings";
 import ErrorBoundary from "../components/ErrorBoundary";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchPosts, increment } from "../../core/store/Post.slice";
-import selectPaginatedPosts from "../../core/selectors/selectPaginatedPosts";
-import selectPostsCounter from "../../core/selectors/selectPostsCounter";
+import usePosts from "../../core/hooks/usePosts";
 
 
 export default function Home() {
   usePageTitle('Home');
-  const dispatch = useDispatch();
-  const paginatedPosts = useSelector(selectPaginatedPosts);
-  const postsCounter = useSelector(selectPostsCounter);
+  const { paginatedPosts, loading, fetchPosts } = usePosts();
 
   return <DefaultLayout>
-    <button onClick={() => dispatch(fetchPosts({ page: 0 }))}>
+    <button onClick={() => fetchPosts({ page: 0 })}>
       disparar ação
     </button>
-    {
-      paginatedPosts?.map(post => <li>{post.title}</li>)
-    }
-    <button onClick={() => dispatch(increment())}>
-      disparar incrementar
-    </button>
-      { postsCounter }
+    { loading ? 'carregando...' : '' }
+    { paginatedPosts?.map(post => <li>{post.title}</li>)}
+    <hr />
     <div style={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', alignItems: 'center', gap: '32px' }}>
       <ErrorBoundary component="top tags">
         <UserTopTags />
