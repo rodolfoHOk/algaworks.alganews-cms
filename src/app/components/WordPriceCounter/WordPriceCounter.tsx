c862e8ebdbeef13eb329605d7229bae8be0f9670
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-11-08 22:58:28 -0300

    fix: word price counter in one word to be singular

--- src/app/components/WordPriceCounter/WordPriceCounter.tsx
+++ src/app/components/WordPriceCounter/WordPriceCounter.tsx
@@ -9,18 +9,20 @@ function WordPriceCounter(props: WordPriceCounterProps) {
   if (props.wordCount < 0)
     throw Error('A quantidade de palavras não pode ser negativa');
 
-  return <WPC.Wrapper>
-    <WPC.WordCounter>
-      {props.wordCount} palavras
-    </WPC.WordCounter>
-    <WPC.PricePreview>
-      {(props.wordCount * props.pricePerWord).toLocaleString('pt-br', {
-        style: 'currency',
-        currency: 'BRL',
-        maximumFractionDigits: 2
-      })}
-    </WPC.PricePreview>
-  </WPC.Wrapper>
+  return (
+    <WPC.Wrapper>
+      <WPC.WordCounter>
+        {props.wordCount} {props.wordCount === 1 ? 'palavra' : 'palavras'}
+      </WPC.WordCounter>
+      <WPC.PricePreview>
+        {(props.wordCount * props.pricePerWord).toLocaleString('pt-br', {
+          style: 'currency',
+          currency: 'BRL',
+          maximumFractionDigits: 2,
+        })}
+      </WPC.PricePreview>
+    </WPC.Wrapper>
+  );
 }
 
 export default WordPriceCounter;
